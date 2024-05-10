2827205e6b0127e21bba2973183beeb20799e3b5
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-05-10 13:44:25 -0500

    update

---
+++ objects/items/food/food.gd
@@ -0,0 +1,1 @@
+class_name food extends item
--- objects/items/slimes/slime.gd
+++ objects/items/slimes/slime.gd
@@ -4,6 +4,21 @@ class_name slime extends item
 var ai_timer_max = 2
 var ai_timer = ai_timer_max - randi() % 5
 var dir = Vector2()
+var diet
+var fav
+var agitation = 0
+var hunger = 100
+
+enum STATES{
+	ELATED,
+	HAPPY,
+	HUNGRY,
+	AGITATED
+}
+
+func _init(given_diet: Array = ["Meat", "Veggi", "Fruit"], fave_food: String = ""):
+	diet = given_diet
+	fav = fave_food
 
 
 func _physics_process(delta):
@@ -12,12 +27,19 @@ func _physics_process(delta):
 		
 	
 	if is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		var towards = Vector2()
+		for food in get_tree().get_nodes_in_group("Food"):
+			if $Area2D.overlaps_body(food):
+				towards = food.global_position-self.global_position
+	
 		ai_timer = clamp(ai_timer-delta, 0, ai_timer_max)
 		if ai_timer==0:
-			velocity=Vector2(randf_range(-1, 1), randf_range(0, 2)).normalized()*JUMP_VELOCITY
+			if towards == Vector2():
+				velocity=(Vector2(randf_range(-1, 1), randf_range(0, 1.5))).normalized()*JUMP_VELOCITY
+			else:
+				velocity = (towards+Vector2(0, -1)).normalized()*JUMP_VELOCITY
 			ai_timer=ai_timer_max
-			
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 
