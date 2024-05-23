d3c3ab29244dbea971247071649f696d0be5b6b4
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-05-23 09:35:24 -0500

    update

--- Spawner.gd
+++ Spawner.gd
@@ -4,18 +4,21 @@ extends Node2D
 @export var slimes: Array = []
 
 
-#func _process(delta):
-#	for player in get_tree().get_nodes_in_group("Player"):
-#		if $Area2D.overlaps_body(player) and len($Area2D.get_overlapping_bodies())<10:
-#			var shoot = load(slimes[randi_range(0, len(slimes)-1)]).instantiate()
-#			shoot.global_position = self.global_position
-#			$"..".add_child.call_deferred(shoot)
+func _process(delta):
+	for player in get_tree().get_nodes_in_group("Player"):
+		if $PlayerDetection.overlaps_body(player) and len($SlimeDetection.get_overlapping_bodies())<10:
+			print("entered")
+			var shoot = load(slimes[randi_range(0, len(slimes)-1)]).instantiate()
+			shoot.global_position = self.global_position
+			$"../..".add_child.call_deferred(shoot)
 
 
 func _on_area_2d_body_entered(body):
-	for player in get_tree().get_nodes_in_group("Player"):
-		if body == player and len($Area2D.get_overlapping_bodies())<10:
-			var shoot = load(slimes[randi_range(0, len(slimes)-1)]).instantiate()
-			shoot.global_position = self.global_position
-			$"..".add_child.call_deferred(shoot)
+	pass
+#	for player in get_tree().get_nodes_in_group("Player"):
+#		if body == player and len($Area2D.get_overlapping_bodies())<10:
+#			print("entered")
+#			var shoot = load(slimes[randi_range(0, len(slimes)-1)]).instantiate()
+#			shoot.global_position = self.global_position
+#			$"../..".add_child.call_deferred(shoot)
 			
