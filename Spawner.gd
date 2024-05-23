ce3ccd70efa3f207c4ebde3a88f363cd72356a26
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-05-23 13:43:29 -0500

    update

--- Spawner.gd
+++ Spawner.gd
@@ -1,16 +1,19 @@
 extends Node2D
-
+var spit = 1
+var chance = 10
 
 @export var slimes: Array = []
 
 
 func _process(delta):
+	
 	for player in get_tree().get_nodes_in_group("Player"):
-		if $PlayerDetection.overlaps_body(player) and len($SlimeDetection.get_overlapping_bodies())<10:
-			print("entered")
+		
+		if $PlayerDetection.overlaps_body(player) and len(self.get_children())<chance:
+			chance = randi_range(0, 100/(spit*len(self.get_children())))
+			spit+=1
 			var shoot = load(slimes[randi_range(0, len(slimes)-1)]).instantiate()
-			shoot.global_position = self.global_position
-			$"../..".add_child.call_deferred(shoot)
+			$".".add_child.call_deferred(shoot)
 
 
 func _on_area_2d_body_entered(body):
--- plt_player.gd
+++ plt_player.gd
@@ -93,7 +93,7 @@ func shoot(selected):
 	
 
 func _physics_process(delta):
-	gun_physics()
+	
 	
 	if Input.is_action_pressed("1"):
 		select = 0
@@ -104,7 +104,7 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("left") and not stuck:
 		shoot(select)
-	
+	gun_physics()
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
